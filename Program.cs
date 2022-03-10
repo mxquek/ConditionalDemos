f155ba0fb1923b6bbf534bde5aa3a3ee8dc19e41
Mabel Quek <mxquek1@gmail.com>
2022-03-10 13:31:39 -0600

    Check if int is parse-able in 'PromptUserForInt' method

--- Program.cs
+++ Program.cs
@@ -6,23 +6,38 @@ namespace ConditionalsLoopMethodsDemo
     {
         static void Main()
         {
-            Console.Write("Do we have power? [y/n]");
+            Console.Write("Do we have power? [y/n] ");
             bool hasPower = Console.ReadLine().ToLower() == "y";
 
-            Console.Write("Do we have paper? [y/n]");
+            Console.Write("Do we have paper? [y/n] ");
             bool hasPaper = Console.ReadLine().ToLower() == "y";
 
-            Console.Write("What is the ink level?");
-            int inkLevel = int.Parse(Console.ReadLine());
+            int inkLevel = PromptUserForInt("What is the ink level? ");
+
+            string name = PromptUser("What is your name? ");
 
             PrintDoc(hasPower, hasPaper, inkLevel);
         }
 
-        //access modifier - static - return type - name (parms in here)
-
         private static void PrintDoc(bool hasPower, bool hasPaper, int inkLevel)
         {
             Console.WriteLine(hasPaper && hasPower && inkLevel >= 10 ? "Printing" : "Unable to print");
+        }
+
+        private static string PromptUser(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+
+        private static int PromptUserForInt(string message)
+        {
+            int result;
+            while (!int.TryParse(PromptUser(message), out result))
+            {
+                PromptUser("Invalid Input! Press any key to continue.");
+            }
+            return result;
         }
     }
 }
